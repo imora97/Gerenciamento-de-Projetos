ec1bf4a0e4be8ad94701a9b95799e292906731aa
imora97 <isarodrigues15@hotmail.com>
2022-01-13 02:47:26 -0300

    Update proj_dao.py

--- model/proj_dao.py
+++ model/proj_dao.py
@@ -4,5 +4,5 @@ def listaAll():
     
     lista = []
     for i in range(10):
-        lista.append(Projeto("Projeto "+str(i), "Descrição "+str(i), [i]))
+        lista.append(Projeto("Projeto "+str(i), "Descrição "+str(i), []))
     return lista

bbdf48a55d2400cbc82a1b5d3888b8ffd6abf66f
imora97 <isarodrigues15@hotmail.com>
2022-01-17 19:48:26 -0300

    .

--- controller/colaborador.py
+++ controller/colaborador.py
@@ -1,7 +1,37 @@
 from qt_core import *
+import model.colab_dao as ColabDAO
+import model.colab as Colab
 
 class Colaborador(QWidget):
     
     def __init__(self):
         super().__init__()
         uic.loadUi('view/colaborador.ui', self)
+
+        # informações do colaborador
+        self.nome_colab.setText(ColabDAO.nome)
+        self.email_colab.setText(ColabDAO.email)
+
+        # ações botões
+        self.excluir_button.clicked.connect(self.exclui_colab)
+        self.limpar_button.clicked.connect(self.limpa_campo)
+        self.salvar_button.clicked.connect(self.salva_colab)
+
+    def exclui_colab(self):
+        self.tableWidget.insertWidget(0, Colaborador())
+        self.tableWidget.setCurrentIndex(0)
+
+    def limpa_campo(self):
+        self.tableWidget.insertWidget(0, Colaborador())
+        self.tableWidget.setCurrentIndex(0)
+
+    def salva_colab(self):
+        nome = self.nome.text()
+        email = self. email.text()
+
+        nv_colab = Colab(nome, email)
+
+        ColabDAO.adicionar(nv_colab)
+        
+        self.tableWidget.insertWidget(0, Colaborador())
+        self.tableWidget.setCurrentIndex(0)
--- model/colab_dao.py
+++ model/colab_dao.py
@@ -1,1 +1,8 @@
 from model.colab import Colab
+
+def lista_colabAll():
+
+    lista_colab = []
+    for c in range(10):
+        lista_colab.append(Colab("Colaborador "+str(c), "Descrição "+str(c), []))
+    return lista_colab
